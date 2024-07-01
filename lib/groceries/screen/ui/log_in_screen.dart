6e8c9bd708b93524ecfcd9e8f3af757927260f07
amitpaju0001 <amitpaju0001@gmail.com>
2024-07-01 17:49:56 +0530

    commit

--- lib/groceries/screen/ui/log_in_screen.dart
+++ lib/groceries/screen/ui/log_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_groceries/groceries/screen/ui/home_screen.dart';
 import 'package:online_groceries/groceries/shared/model/network_image_model.dart';
 
 class LogInScreen extends StatefulWidget {
@@ -9,27 +10,144 @@ class LogInScreen extends StatefulWidget {
 }
 
 class _LogInScreenState extends State<LogInScreen> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isPassword = false;
+  bool navigate = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Image.network(
-              NetworkImageModel.imageAppBar,
-              height: 20,
-              width: 20,
-            ),
-            SizedBox(height: 32,),
-            ListTile(
-              title: Text('Loging',style: TextStyle(
-                fontWeight: FontWeight.bold
-              ),),
-              subtitle: Text('Enter your email and password'),
-            ),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Padding(
+              padding:  const EdgeInsets.only(left: 16, right: 16,top: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 36),
+                  Center(
+                    child: Image.network(
+                      NetworkImageModel.imageAppBar,
+                      height: 40,
+                      width: 40,
+                    ),
+                  ),
+                  const SizedBox(height: 100),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Loging',
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Enter your email and password',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, right: 8,top: 16),
+                    child: TextFormField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          hintText: 'Enter your Email',
 
-          ],
+                        ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter Email';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          navigate = formKey.currentState?.validate() ?? false;
+                        });
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, right: 8,top: 16),
+                    child: TextFormField(
+                      controller: passwordController,
+                      obscureText: !isPassword,
+                      decoration: InputDecoration(
+                        hintText: 'Enter Your password',
+                        labelText: 'Password',
+                        suffixIcon: IconButton(
+                          icon: Icon(isPassword
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                          onPressed: () {
+                            setState(() {
+                              isPassword = !isPassword;
+                            });
+                          },
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter password';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          navigate = formKey.currentState?.validate() ?? false;
+                        });
+                      },
+                    ),
+                  ),
+                   const SizedBox(height: 8),
+                  const Align(
+                    alignment: Alignment.centerRight,
+                    child: Text('Forgot password?'),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: navigate
+                          ? () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const HomeScreen(),
+                          ),
+                        );
+                      }
+                      : null,
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 100, vertical: 15),
+                      ),
+                      child: const Text('Log In'),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: TextButton(
+                      onPressed: () {
+                      },
+                      child: const Text(
+                        'Don’t have an account? Signup',
+                        style: TextStyle(color: Colors.green),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
--- lib/groceries/screen/ui/select_location_screen.dart
+++ lib/groceries/screen/ui/select_location_screen.dart
@@ -19,77 +19,79 @@ class _SelectLocationScreenState extends State<SelectLocationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network(
-                'https://cdn.icon-icons.com/icons2/159/PNG/256/google_location_maps_22371.png',
-                height: 200),
-            const SizedBox(height: 16),
-            const Text(
-              "Select Your Location",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              "Select your location in your area",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-            const SizedBox(height: 120),
-            DropdownButtonFormField(
-              value: selectedZone,
-              hint: const Text("Your Zone"),
-              items: zones.map((zone) {
-                return DropdownMenuItem(
-                  value: zone,
-                  child: Text(zone),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  selectedZone = value;
-                });
-              },
-              decoration: const InputDecoration(
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                  'https://cdn.icon-icons.com/icons2/159/PNG/256/google_location_maps_22371.png',
+                  height: 200),
+              const SizedBox(height: 16),
+              const Text(
+                "Select Your Location",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(height: 32),
-            DropdownButtonFormField(
-              value: selectedArea,
-              hint: const Text("Your Area"),
-              items: areas.map((area) {
-                return DropdownMenuItem(
-                  value: area,
-                  child: Text(area),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  selectedArea = value;
-                });
-              },
-              decoration: const InputDecoration(
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              const SizedBox(height: 16),
+              const Text(
+                "Select your location in your area",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, color: Colors.grey),
               ),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => LogInScreen(),));
-              },
-              style: ElevatedButton.styleFrom(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 80, vertical: 16),
+              const SizedBox(height: 120),
+              DropdownButtonFormField(
+                value: selectedZone,
+                hint: const Text("Your Zone"),
+                items: zones.map((zone) {
+                  return DropdownMenuItem(
+                    value: zone,
+                    child: Text(zone),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    selectedZone = value;
+                  });
+                },
+                decoration: const InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                ),
               ),
-              child: const Text("Submit"),
-            ),
-          ],
+              const SizedBox(height: 32),
+              DropdownButtonFormField(
+                value: selectedArea,
+                hint: const Text("Your Area"),
+                items: areas.map((area) {
+                  return DropdownMenuItem(
+                    value: area,
+                    child: Text(area),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    selectedArea = value;
+                  });
+                },
+                decoration: const InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                ),
+              ),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const LogInScreen(),));
+                },
+                style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 80, vertical: 16),
+                ),
+                child: const Text("Submit"),
+              ),
+            ],
+          ),
         ),
       ),
     );
