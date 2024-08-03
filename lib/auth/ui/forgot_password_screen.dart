ae549986d581f68742c18cf7f6b205cdcbec745d
amitpaju0001 <amitpaju0001@gmail.com>
2024-08-03 09:25:58 +0530

    create forgot password screen and create send password reset email for password reset and change in service and provider create a reset button in login screen

---
+++ lib/auth/ui/forgot_password_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:online_groceries/auth/provider/auth_provider.dart';
+import 'package:provider/provider.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Forgot Password'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                hintText: 'Enter your Email',
+              ),
+            ),
+            Consumer<AuthProvider>(
+              builder: (context, provider, child) {
+                return ElevatedButton(
+                  onPressed: () {
+                    provider.resetPassword(emailController.text.trim());
+                  },
+                  child: provider.isLoading
+                      ? const CircularProgressIndicator(
+                    color: Colors.white,
+                  )
+                      : Text('Send Link'),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
